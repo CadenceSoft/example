34c97ccfa8c6aee586809b29540e5a2349f05f4c
Dman89 <Daniel@DanielCudney.com>
2024-02-06 17:52:03 -0800

    Final, I hope...

--- HelloWorldNPM/src/cadso-hello-world-npm.js
+++ HelloWorldNPM/src/cadso-hello-world-npm.js
@@ -1,17 +1,23 @@
 import { createEnhancedElement } from '@servicenow/library-enhanced-element';
 
 
-const HelloWorldNPM = ({ text }) => {
+const HelloWorldNPM = ({ text, properties }) => {
+	const { text: propText = '' } = properties;
 	return (
 		<div className="npm-package-has-been-packaged">
-			Hello World, <bold>{text}</bold>
+			Hello World, <bold>{propText || text}</bold>
 		</div>
 	);
 };
 
 createEnhancedElement("cadso-hello-world-npm", {
 	initialState: {
-		text: "Bob",
+		text: "No Value Provided",
+	},
+	properties: {
+		text: {
+			default: "",
+		},
 	},
 	view: HelloWorldNPM,
 });
